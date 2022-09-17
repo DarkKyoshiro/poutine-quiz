ff4061876c9201a82404f2fb80f04957f6fb87b8
DarkKyoshiro <guillaume.dubois45@gmail.com>
2022-09-17 19:28:01 -0400

    environment use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,24 @@ import { GameComponent } from './game/game.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { QuestionFormComponent } from './question-form/question-form.component';
+import { environment } from 'src/environments/environment';
 
 //For HerokuApp
-const config: SocketIoConfig = { url: 'https://poutinequiz.herokuapp.com/', options: {} };
+//const config: SocketIoConfig = { url: 'https://poutinequiz.herokuapp.com/', options: {} };
 
 //For Local Server
-//const config: SocketIoConfig = { url: 'http://localhost:8080/', options: {} };
+// const config: SocketIoConfig = { url: 'http://localhost:8080/', options: {} };
+
+const config: SocketIoConfig = { url: environment.apiUrl, options: {} };
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AdminComponent,
-    GameComponent
+    GameComponent,
+    QuestionFormComponent
   ],
   imports: [
     BrowserModule,
