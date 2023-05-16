8db5461d9e2f54153298f8a57a2db038dfe4a763
DarkKyoshiro <guillaume.dubois45@gmail.com>
2023-05-15 21:35:46 -0400

    update of remontada/degringolada logic

--- src/app/admin-modules/admin-stats/admin-stats.component.ts
+++ src/app/admin-modules/admin-stats/admin-stats.component.ts
@@ -294,13 +294,66 @@ export class AdminStatsComponent implements OnInit {
     return bestTeam + " (" + Math.trunc(bestScore)/1000 + " sec)";
   }
 
-  remontada(): string {
+  // remontada(): string {
+  //   let teamsRd1 = new Map<string, number>()
+  //   let teamsRd2 = new Map<string, number>()
+
+  //   let bestTeam: string = "";
+  //   let bestScore: number = -999;
+  //   let bestTotal: number = -999;
+
+  //   this.answers.forEach(answer => {
+  //     if(!teamsRd1.has(answer.teamName)) { teamsRd1.set(answer.teamName, 0) }
+  //     if(!teamsRd2.has(answer.teamName)) { teamsRd2.set(answer.teamName, 0) }
+  //     if(this.questions[answer.questionID - 1].round === 1) {
+  //       teamsRd1.set(answer.teamName, teamsRd1.get(answer.teamName) + answer.points + answer.bonus + answer.bonusWrongAnswers)
+  //       teamsRd2.set(answer.teamName, teamsRd2.get(answer.teamName) + answer.points + answer.bonus + answer.bonusWrongAnswers)
+  //     } else {
+  //       teamsRd2.set(answer.teamName, teamsRd2.get(answer.teamName) + answer.points + answer.bonus + answer.bonusWrongAnswers)
+  //     }
+  //   })
+
+  //   let teamsRd1Sorted = new Map([...teamsRd1].sort((a,b) => { return b[1]-a[1] }))
+  //   let teamsRd2Sorted = new Map([...teamsRd2].sort((a,b) => { return b[1]-a[1] }))
+
+  //   let teamsRd1Ranked = new Map<string, number>()
+  //   let teamsRd2Ranked = new Map<string, number>()
+
+  //   let i: number = 1
+  //   for (let [key, value] of teamsRd1Sorted) {
+  //     teamsRd1Ranked.set(key, i)
+  //     i++
+  //   }
+
+  //   i = 1
+  //   for (let [key, value] of teamsRd2Sorted) {
+  //     teamsRd2Ranked.set(key, i)
+  //     i++
+  //   }
+
+  //   if(teamsRd1Ranked.size > 0 && teamsRd2Ranked.size > 0) {
+  //     for (let [key, value] of teamsRd1Ranked) {
+  //       if(value - teamsRd2Ranked.get(key) > bestScore) { 
+  //         bestTeam = key
+  //         bestScore = value - teamsRd2Ranked.get(key)
+  //         bestTotal = teamsRd2Ranked.get(key)
+  //       } else if(teamsRd2Ranked.get(key) - value === bestScore && teamsRd2Ranked.get(key) > bestTotal) { 
+  //         bestTeam = key
+  //         bestScore = value - teamsRd2Ranked.get(key)
+  //         bestTotal = teamsRd2Ranked.get(key)
+  //       }
+  //     }
+
+  //     return bestTeam + " (Rd1: " + teamsRd1Ranked.get(bestTeam) + ", Rd2: " + teamsRd2Ranked.get(bestTeam) + ", Diff: " + bestScore + ")";
+  //   }
+  //   else  {
+  //     return 'No one'
+  //   }
+  // }
+
+  remontadaTable(): { name: string; rd1Rank: number; rd1Score: number; rd2Rank: number; rd2Score: number }[] {
     let teamsRd1 = new Map<string, number>()
     let teamsRd2 = new Map<string, number>()
-
-    let bestTeam: string = "";
-    let bestScore: number = -999;
-    let bestTotal: number = -999;
 
     this.answers.forEach(answer => {
       if(!teamsRd1.has(answer.teamName)) { teamsRd1.set(answer.teamName, 0) }
@@ -331,82 +384,92 @@ export class AdminStatsComponent implements OnInit {
       i++
     }
 
-    if(teamsRd1Ranked.size > 0 && teamsRd2Ranked.size > 0) {
-      for (let [key, value] of teamsRd1Ranked) {
-        if(value - teamsRd2Ranked.get(key) > bestScore) { 
-          bestTeam = key
-          bestScore = value - teamsRd2Ranked.get(key)
-          bestTotal = teamsRd2Ranked.get(key)
-        } else if(teamsRd2Ranked.get(key) - value === bestScore && teamsRd2Ranked.get(key) > bestTotal) { 
-          bestTeam = key
-          bestScore = value - teamsRd2Ranked.get(key)
-          bestTotal = teamsRd2Ranked.get(key)
-        }
-      }
-
-      return bestTeam + " (Rd1: " + teamsRd1Ranked.get(bestTeam) + ", Rd2: " + teamsRd2Ranked.get(bestTeam) + ", Diff: " + bestScore + ")";
-    }
-    else  {
-      return 'No one'
-    }
-  }
-
-  degringolada(): string {
-    let teamsRd1 = new Map<string, number>()
-    let teamsRd2 = new Map<string, number>()
-
-    let bestTeam: string = "";
-    let bestScore: number = -999;
-    let bestTotal: number = -999;
-
-    this.answers.forEach(answer => {
-      if(!teamsRd1.has(answer.teamName)) { teamsRd1.set(answer.teamName, 0) }
-      if(!teamsRd2.has(answer.teamName)) { teamsRd2.set(answer.teamName, 0) }
-      if(this.questions[answer.questionID - 1].round === 1) {
-        teamsRd1.set(answer.teamName, teamsRd1.get(answer.teamName) + answer.points + answer.bonus + answer.bonusWrongAnswers)
-        teamsRd2.set(answer.teamName, teamsRd2.get(answer.teamName) + answer.points + answer.bonus + answer.bonusWrongAnswers)
-      } else {
-        teamsRd2.set(answer.teamName, teamsRd2.get(answer.teamName) + answer.points + answer.bonus + answer.bonusWrongAnswers)
-      }
-    })
-
-    let teamsRd1Sorted = new Map([...teamsRd1].sort((a,b) => { return b[1]-a[1] }))
-    let teamsRd2Sorted = new Map([...teamsRd2].sort((a,b) => { return b[1]-a[1] }))
-
-    let teamsRd1Ranked = new Map<string, number>()
-    let teamsRd2Ranked = new Map<string, number>()
-
-    let i: number = 1
-    for (let [key, value] of teamsRd1Sorted) {
-      teamsRd1Ranked.set(key, i)
-      i++
-    }
-
-    i = 1
-    for (let [key, value] of teamsRd2Sorted) {
-      teamsRd2Ranked.set(key, i)
-      i++
-    }
+    let finalTeams: { name: string; rd1Rank: number; rd1Score: number; rd2Rank: number; rd2Score: number }[] = []
 
     if(teamsRd1Ranked.size > 0 && teamsRd2Ranked.size > 0) {
       for (let [key, value] of teamsRd1Ranked) {
-        if(teamsRd2Ranked.get(key) - value > bestScore) { 
-          bestTeam = key
-          bestScore = teamsRd2Ranked.get(key) - value
-          bestTotal = teamsRd2Ranked.get(key)
-        } else if(teamsRd2Ranked.get(key) - value === bestScore && teamsRd2Ranked.get(key) > bestTotal) { 
-          bestTeam = key
-          bestScore = teamsRd2Ranked.get(key) - value
-          bestTotal = teamsRd2Ranked.get(key)
-        }
+        finalTeams.push({
+          name: key,
+          rd1Rank: value,
+          rd1Score: teamsRd1.get(key),
+          rd2Rank: teamsRd2Ranked.get(key),
+          rd2Score: teamsRd2.get(key)
+        })
       }
 
-      return bestTeam + " (Rd1: " + teamsRd1Ranked.get(bestTeam) + ", Rd2: " + teamsRd2Ranked.get(bestTeam) + ", Diff: " + bestScore + ")";
+      finalTeams = finalTeams.slice().sort((a,b) => { return (b.rd2Rank - b.rd1Rank) - (a.rd2Rank - a.rd1Rank) })
+      finalTeams = finalTeams.slice().sort((a,b) => { return (b.rd2Score - b.rd1Score)/b.rd1Score - (a.rd2Score - a.rd1Score)/a.rd1Score })
+
+      return finalTeams;
     }
     else  {
-      return 'No one'
+      finalTeams.push({
+        name: 'No one',
+        rd1Rank: 0,
+        rd1Score: 0,
+        rd2Rank: 0,
+        rd2Score: 0
+      })
+      return finalTeams;
     }
   }
+
+  // degringolada(): string {
+  //   let teamsRd1 = new Map<string, number>()
+  //   let teamsRd2 = new Map<string, number>()
+
+  //   let bestTeam: string = "";
+  //   let bestScore: number = -999;
+  //   let bestTotal: number = -999;
+
+  //   this.answers.forEach(answer => {
+  //     if(!teamsRd1.has(answer.teamName)) { teamsRd1.set(answer.teamName, 0) }
+  //     if(!teamsRd2.has(answer.teamName)) { teamsRd2.set(answer.teamName, 0) }
+  //     if(this.questions[answer.questionID - 1].round === 1) {
+  //       teamsRd1.set(answer.teamName, teamsRd1.get(answer.teamName) + answer.points + answer.bonus + answer.bonusWrongAnswers)
+  //       teamsRd2.set(answer.teamName, teamsRd2.get(answer.teamName) + answer.points + answer.bonus + answer.bonusWrongAnswers)
+  //     } else {
+  //       teamsRd2.set(answer.teamName, teamsRd2.get(answer.teamName) + answer.points + answer.bonus + answer.bonusWrongAnswers)
+  //     }
+  //   })
+
+  //   let teamsRd1Sorted = new Map([...teamsRd1].sort((a,b) => { return b[1]-a[1] }))
+  //   let teamsRd2Sorted = new Map([...teamsRd2].sort((a,b) => { return b[1]-a[1] }))
+
+  //   let teamsRd1Ranked = new Map<string, number>()
+  //   let teamsRd2Ranked = new Map<string, number>()
+
+  //   let i: number = 1
+  //   for (let [key, value] of teamsRd1Sorted) {
+  //     teamsRd1Ranked.set(key, i)
+  //     i++
+  //   }
+
+  //   i = 1
+  //   for (let [key, value] of teamsRd2Sorted) {
+  //     teamsRd2Ranked.set(key, i)
+  //     i++
+  //   }
+
+  //   if(teamsRd1Ranked.size > 0 && teamsRd2Ranked.size > 0) {
+  //     for (let [key, value] of teamsRd1Ranked) {
+  //       if(teamsRd2Ranked.get(key) - value > bestScore) { 
+  //         bestTeam = key
+  //         bestScore = teamsRd2Ranked.get(key) - value
+  //         bestTotal = teamsRd2Ranked.get(key)
+  //       } else if(teamsRd2Ranked.get(key) - value === bestScore && teamsRd2Ranked.get(key) > bestTotal) { 
+  //         bestTeam = key
+  //         bestScore = teamsRd2Ranked.get(key) - value
+  //         bestTotal = teamsRd2Ranked.get(key)
+  //       }
+  //     }
+
+  //     return bestTeam + " (Rd1: " + teamsRd1Ranked.get(bestTeam) + ", Rd2: " + teamsRd2Ranked.get(bestTeam) + ", Diff: " + bestScore + ")";
+  //   }
+  //   else  {
+  //     return 'No one'
+  //   }
+  // }
 
   getRanks(teamName: string): number {
     let teamsArray: number[] = []
