489e9b05aeb4a8ffaddeafc50f1ec353cbd2c29c
DarkKyoshiro <guillaume.dubois45@gmail.com>
2024-04-18 19:39:40 -0400

    Add buttons to allow easy login of existing teams on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Socket } from 'ngx-socket-io';
+import { Team } from '../models/team.model';
 
 @Component({
   selector: 'app-home',
@@ -10,16 +12,31 @@ import { Router } from '@angular/router';
 export class HomeComponent implements OnInit {
   teamName!: string;
   state!: string;
+  teams: Team[] = [];
 
-  constructor(private router: Router) { }
+  constructor(
+    private socket: Socket,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.state = this.router.url
+
+    //Request teams refreshment
+    this.socket.emit('refresh-teams')
+
+    //Teams refreshment
+    this.socket.on('send-teams', (data: any) => {
+      this.teams = Object.keys(data).map(key => data[key]);
+    });
   }
 
   onJoin(form: NgForm): void {
     if(form.value.teamName) {
       this.router.navigateByUrl('game/' + form.value.teamName);
     }
+  }
+
+  onFillTeam(teamName: string): void {
+    this.router.navigateByUrl('game/' + teamName);
   }
 }
